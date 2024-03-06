782a6245052824d7c47c0cf90aedcf5272cdc463
MohitAgrawal404 <HiMohitA@gmail.com>
2024-03-06 13:14:06 -0800

    Earth model working

--- prog5/src/script.js
+++ prog5/src/script.js
@@ -19,7 +19,7 @@ const scene = new THREE.Scene();
  */
 
 const gltfLoader = new GLTFLoader();
-gltfLoader.load("/models/Duck/glTF/Duck.gltf", (gltf) => {
+gltfLoader.load("/models/Earth/Earth.glb", (gltf) => {
   scene.add(gltf.scene.children[0]);
 });
 /**
@@ -35,24 +35,29 @@ const floor = new THREE.Mesh(
 );
 floor.receiveShadow = true;
 floor.rotation.x = -Math.PI * 0.5;
-scene.add(floor);
+//scene.add(floor);
 
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 2.4);
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.0);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.set(1024, 1024);
-directionalLight.shadow.camera.far = 15;
-directionalLight.shadow.camera.left = -7;
-directionalLight.shadow.camera.top = 7;
-directionalLight.shadow.camera.right = 7;
-directionalLight.shadow.camera.bottom = -7;
-directionalLight.position.set(5, 5, 5);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.4);
+// directionalLight.castShadow = true;
+// directionalLight.shadow.mapSize.set(1024, 1024);
+// directionalLight.shadow.camera.far = 15;
+// directionalLight.shadow.camera.left = -7;
+// directionalLight.shadow.camera.top = 7;
+// directionalLight.shadow.camera.right = 7;
+// directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(15, 15, 15);
 scene.add(directionalLight);
+
+const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1.4);
+
+directionalLight2.position.set(-15, -15, -15);
+scene.add(directionalLight2);
 
 /**
  * Sizes
@@ -91,7 +96,7 @@ scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
-controls.target.set(0, 0.75, 0);
+controls.target.set(0, 0, 0);
 controls.enableDamping = true;
 
 /**
